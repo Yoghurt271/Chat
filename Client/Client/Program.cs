ec67aa10895ca6db14c718fcc840d971a619f644
Yoghurt271 <112902772+Yoghurt271@users.noreply.github.com>
2022-10-04 02:08:45 +0300

    Add files via upload

---
+++ Client/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Connect();
+            class1.SendMessage();
+            class1.ReceiveMessage();
+            class1.Disconnect();
+        }
+    }
+}
